c4c6e103c49da2fd1d351112bdeefe9fe8872a40
KalitaAleks <kalita.aliksandr@mail.ru>
2015-11-12 17:02:28 +0600

    Добавить функции сортировки

--- Omgtu.CollectionSortExcercise/Omgtu.CollectionSortExcercise/PersonComparer.cs
+++ Omgtu.CollectionSortExcercise/Omgtu.CollectionSortExcercise/PersonComparer.cs
@@ -3,27 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Omgtu.CollectionSortExcercise
-{
-    public static class PersonComparer
     {
-        public static int CompareByName(Person leftPerson, Person rightPerson)
+    public static class PersonComparer
         {
-            //TODO: Добавить реализацию сравнения по именам
-            throw new NotImplementedException();
-        }
+        public static int CompareByName(Person leftPerson, Person rightPerson)
+            {
+            if (rightPerson == null && leftPerson == null)
+                {
+                return 0;
+                }
+            if (leftPerson == null)
+                {
+                return -1;
+                }
+            if (rightPerson == null)
+                {
+                return 1;
+                }
+
+            return leftPerson.Name.CompareTo(rightPerson.Name);
+
+
+
+            }
 
         public static int CompareBySurname(Person leftPerson, Person rightPerson)
-        {
-            //TODO: Добавить реализацию сравнения по именам
-            throw new NotImplementedException();
-        }
+            {
+            return leftPerson.Surname.CompareTo(rightPerson.Surname);
+
+
+            }
 
         public static int CompareByGroupNameSurname(Person leftPerson, Person rightPerson)
-        {
-            //TODO: Добавить реализацию сравнения по именам
-            throw new NotImplementedException();
+            {
+
+
+            if (leftPerson.Group.CompareTo(rightPerson.Group) == 0)
+                {
+                if (CompareByName(leftPerson, rightPerson) == 0)
+                { return CompareBySurname(leftPerson, rightPerson); }
+                else return CompareByName(leftPerson, rightPerson);
+
+                }
+            else
+                return leftPerson.Group.CompareTo(rightPerson.Group);
+
+
+
+
+
+
+
+            }
         }
     }
-}
